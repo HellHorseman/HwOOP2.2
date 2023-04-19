0413bf95abc68e6f975d97f88e919b09f35860bb
HellHorseman <duke.sheffer@yandex.ru>
2023-04-19 19:02:40 +0400

    New branch + encapsulation

--- src/Truck.java
+++ src/Truck.java
@@ -1,7 +1,28 @@
 public class Truck {
 
-    public String modelName;
-    public int wheelsCount;
+    private String modelName;
+    private int wheelsCount;
+
+    public Truck(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
 
     public void updateTyre() {
         System.out.println("Меняем покрышку");
